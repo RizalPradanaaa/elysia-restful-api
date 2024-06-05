5a1824c27b33463c2d3d62af648e080a148d04bd
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-06-05 13:44:16 +0700

    Add Post

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -19,3 +19,30 @@ export async function getPosts() {
     console.error(`Error getting posts: ${e}`);
   }
 }
+
+/**
+ * Creating a post
+ */
+export async function createPost(options: { title: string; content: string }) {
+  try {
+    //get title and content
+    const { title, content } = options;
+
+    //create data post
+    const post = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+      },
+    });
+
+    //return response json
+    return {
+      success: true,
+      message: "Post Created Successfully!",
+      data: post,
+    };
+  } catch (e: unknown) {
+    console.error(`Error creating post: ${e}`);
+  }
+}
